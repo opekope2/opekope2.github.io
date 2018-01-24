5aacaa16045365b1c4cf74d0a666971fa3774e9a
opekope2 <opekope2@gmail.com>
2018-01-24 14:24:28 +0100

    Update moza.js

--- moza.js
+++ moza.js
@@ -6,9 +6,17 @@ avgbtn.addEventListener("click", avg, false);
 loc.appendChild(avgbtn);
 function avg()
 {
+	var values = [];
+	values["rgb(34, 30, 31)"] = 1;
+	values["rgb(51, 170, 0)"] = 0.5;
+	values["rgb(49, 102, 255)"] = 0.5;
+	values["rgb(34, 30, 31)"] = 1;
+	values["rgb(52, 0, 150)"] = 1;
+	values["rgb(153, 51, 0)"] = 1.5;
+	values["rgb(255, 17, 0)"] = 2;
+	values.push({});
 	var avgwin = window.open("", "Átlag", "width=400,height=300");
 	avgwin.document.write("Az átlag itt jelenik meg");
-	
 	var statistics = docuemnt.getElementById("statistics");
 	for (var i = 0, row; row = statistics.rows[i]; i++)
 	{

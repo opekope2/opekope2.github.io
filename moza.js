ceb22d5b7f3c70e7524bc6b9473ab1c24813367f
opekope2 <opekope2@gmail.com>
2018-01-24 09:42:07 +0100

    Update moza.js

--- moza.js
+++ moza.js
@@ -8,5 +8,17 @@ function avg()
 {
 	var avgwin = window.open("", "Átlag", "width=400,height=300");
 	avgwin.document.write("Az átlag itt jelenik meg");
-	alert("atlag");
+	
+	var statistics = docuemnt.getElementById("statistics");
+	for (var i = 0, row; row = statistics.rows[i]; i++)
+	{
+		var grades = [];
+		for (var j = 0, col; col = row.cells[j]; j++)
+		{
+			for (var k = 0, child; child = col.childNodes[k]; k++)
+			{
+				//grades.push({grade:5,value:2});
+   			}
+   		}
+	}
 }

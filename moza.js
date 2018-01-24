6610aec243aab87a94e67cae2a9dafd0eb5ed0ff
opekope2 <opekope2@gmail.com>
2018-01-24 15:27:37 +0100

    Update moza.js

--- moza.js
+++ moza.js
@@ -16,36 +16,38 @@ function disp()
 	values["rgb(255, 17, 0)"] = 2;
 	var avgwin = window.open("", "Átlag", "width=400,height=300");
 	var statistics = document.getElementById("statistics");
-	for (var i = 0, row; row = statistics.rows[i]; i++)
+	for (var i = 2, row; row = statistics.rows[i]; i++)
 	{
+		var grades = [];
 		for (var j = 1, col; col = row.cells[j]; j++)
 		{
-			var grades = [];
-			for (var k = 0, child; child = col.childNodes[k]; k++)
+			for (var k = 0, child; child = col.children[k]; k++)
 			{
 				grades.push({ grade: child.innerText, multiplier: values[child.style.color] });
    			}
-			var div = "";
-			div += "<div style='border-style: solid; max-width: 400px;'>";
-			div += "<h6 style='text-align: center;'>" + row.cells[0] + "</h6>";
-			div += "<table border='0'>";
-			div += "<tr><td>Matematikai átlag:</td><td>" + avg(grades).normal + "</td></tr>";
-			div += "<tr><td>Súlyozott átlag:</td><td>" + avg(grades).weighted + "</td></tr>";
-			div += "</table>";
-			div += "</div>";
-			avgwin.document.write(div);
    		}
+		var div = "";
+		div += "<div style='border-style: solid; max-width: 400px;'>";
+		div += "<h6 style='text-align: center;'>" + row.cells[0].innerText + "</h6>";
+		div += "<table border='0'>";
+		div += "<tr><td>Matematikai átlag:</td><td>" + avg(grades).normal + "</td></tr>";
+		div += "<tr><td>Súlyozott átlag:</td><td>" + avg(grades).weighted + "</td></tr>";
+		div += "</table>";
+		div += "</div>";
+		avgwin.document.write(div);
 	}
 }
 function avg(arr)
 {
-	var all = 0;
-	var nums = 0;
+	var normal = parseInt(0);
+	var weighted = parseInt(0);
+	var nums = parseInt(0);
 	for(var v = 0; v < arr.length; v++)
 	{
-		all += arr[v].grade;
-		nums += arr[v].multiplier;
+		weighted += parseInt(arr[v].grade) * parseInt(arr[v].multiplier);
+		nums += parseInt(arr[v].multiplier);
+		normal += parseInt(arr[v].grade);
 	}
-	var ret = { normal: Number(all)/arr.length, weighted: Number(all) / Number(nums) };
+	var ret = { normal: normal / arr.length, weighted: weighted / nums };
 	return ret;
 }

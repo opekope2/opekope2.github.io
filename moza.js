6aed407cccce543c8348225c41a7a8295716ba8e
opekope2 <opekope2@gmail.com>
2018-01-24 09:26:23 +0100

    Update moza.js

--- moza.js
+++ moza.js
@@ -1,11 +1,12 @@
 var loc = document.getElementById("jegyhelpgomb").parentElement;
 var avgbtn = document.createElement("button");
-avgbtn.value = "Súlyozott átlag";
+avgbtn.innerText = "Súlyozott átlag";
 avgbtn.className = "css3_button blue";
 avgbtn.onclick = avg;
 loc.appendChild(avgbtn);
 function avg()
 {
-	var avgwin = null;
+	var avgwin = window.open("", "Átlag", "width=400,height=300");
+	avgwin.document.write("Az átlag itt jelenik meg");
 	alert("atlag");
 }

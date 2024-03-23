0695d6f82166af19b8f2c5349cd00e87dbdbd0bd
opekope2 <opekope2@gmail.com>
2024-03-23 19:31:24 +0100

    Add card component

---
+++ src/lib/Card.svelte
@@ -0,0 +1,38 @@
+<article class="card">
+    {#if $$slots.header}
+        <header>
+            <slot name="header" />
+        </header>
+    {/if}
+
+    <slot />
+
+    {#if $$slots.footer}
+        <footer>
+            <slot name="footer" />
+        </footer>
+    {/if}
+</article>
+
+<style lang="scss">
+    @import "../scss/fancy/fancy.scss";
+
+    .card {
+        position: relative;
+
+        &::before {
+            @include fancy;
+            content: "";
+            position: absolute;
+            z-index: -1;
+            inset: -1px;
+            border-radius: inherit;
+            transition: all 0.3s;
+        }
+
+        &:hover::before,
+        &:focus-within::before {
+            filter: blur(2rem);
+        }
+    }
+</style>

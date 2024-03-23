f7703634d81661d70018b591fdfa8e1445ea49de
opekope2 <opekope2@gmail.com>
2024-03-23 19:31:38 +0100

    Add icon and logo components

---
+++ src/lib/icon/MaterialIcon.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    export let name: string;
+</script>
+
+<span class="material-icons">{name}</span>
+
+<style lang="scss">
+    .material-icons {
+        font-size: 1em;
+    }
+</style>

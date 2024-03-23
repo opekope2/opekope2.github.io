88b84a43c67b689dbc71f4395b7343278b76f08a
opekope2 <opekope2@gmail.com>
2024-03-23 18:41:27 +0100

    Add & import styles

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import "../scss/app.scss";
+    import "../scss/fancy/fancy.scss";
+    import "../scss/fancy/fancy-text.scss";
+    import "../scss/fancy/fancy-button.scss";
+    import "@picocss/pico/css/pico.min.css";
+</script>
+
+<slot />
